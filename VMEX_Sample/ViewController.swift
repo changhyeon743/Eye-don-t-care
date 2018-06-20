25e3e5eec855951442be7b4ec0098e6f5292740c
David Lee <gosoohro@gmail.com>
2018-06-20 15:34:53 +0900

    update sample code

--- VMEX_Sample/ViewController.swift
+++ VMEX_Sample/ViewController.swift
@@ -1,25 +1,81 @@
 //
 //  ViewController.swift
 //  VMEX_Sample
-//
-//  Created by 이다빈 on 2018. 6. 20..
-//  Copyright © 2018년 이다빈. All rights reserved.
-//
+
 
 import UIKit
+// import eye-tracking framework
+import VMEX
 
 class ViewController: UIViewController {
 
+    // initialize eye-tracking session
+    let sessionHandler = SessionHandler()
+     // Bool that check session open
+    var sessionOpened : Bool = false
+    
+    // gaze view
+    var gazePt : UIView?
+    // gaze view size
+    var gazeSize : CGSize = CGSize(width: 10, height: 10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
+    
+    // Resume or start eye-tracking depending on whether openSession method is called.
+    override func viewWillAppear(_ animated: Bool) {
+        if sessionOpened {
+            sessionHandler.resumeSession()
+        }else{
+            //portraid demo
+            sessionHandler.openSession(receiver: self, root: self.view, mode: ScreenMode.Portrait, isCalibration: true)
+            sessionOpened = true
+            sessionHandler.startSession()
+        }
+    }
+    
+    // If the view disappears, the eye-tracking stops.
+    override func viewWillDisappear(_ animated: Bool) {
+        if sessionOpened {
+            sessionHandler.pauseSession()
+        }
+    }
+    
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    // kill sessionHandler
+    deinit {
+        sessionHandler.closeSession()
     }
 
 
+}
+
+extension ViewController : Receiver {
+    
+    // receive gaze point (Y coordinate)
+    func receive(value: [Double], isTracking: Bool) {
+        DispatchQueue.main.async {
+            if isTracking {
+                self.gazePt?.frame.origin.y = CGFloat(value[0]) - self.gazeSize.height/2
+            }
+        }
+    }
+    // called when calibraiton is done. At this time, create a view to indicate gaze coordinate
+    func calibrationFinished() {
+        gazePt = UIView(frame: CGRect(x: self.view.frame.width/2 - gazeSize.width/2, y: self.view.frame.height/2 - gazeSize.height/2, width: gazeSize.width, height: gazeSize.height))
+        gazePt!.layer.cornerRadius = gazeSize.width/2
+        gazePt!.backgroundColor = UIColor.red
+        self.view.addSubview(gazePt!)
+        
+    }
+    
+    
 }
 
---
+++ readme.md
@@ -0,0 +1,37 @@
+
+<br>
+<br>
+
+<p align="center">
+<img src="http://visual.camp/wp-content/uploads/2017/01/logo.png" alt="VisualCamp">
+</p>
+
+
+## VMEX (Visualcamp Moblie Eyetracking Extension)
+
+* **Xcode:** 9.3+
+* **OS:** iOS 11+
+* **Languages:** Swift 4.1+
+* **Important:** This SDK is not built on the simulator but  only built on real devices.
+
+## Getting started
+
+
+### 1. TARGETS -> General -> Embedded Binaries -> Add VMEX.framework
+
+![improt_framework](https://user-images.githubusercontent.com/16757294/41086621-89c5fe74-6a75-11e8-809c-67f7df6ed1c5.png)
+
+### 2. TARGETS -> General -> Set Device Orientation
+
+![DeviceOrientation settings](https://user-images.githubusercontent.com/16757294/41573642-a7e98226-73b8-11e8-9811-eca473cbe051.png)
+
+
+### 3. TARGETS -> Info -> Add "Privacy - Camera Usage Description"
+
+![Camera setting](https://user-images.githubusercontent.com/16757294/41572794-8f68882c-73b4-11e8-89a1-41ecd453f8b8.png)
+
+
+## Feedback
+
+- Brad [brad@visual.camp](mailto:brad@visual.camp)
+- David [david@visual.camp](mailto:david@visual.camp)
